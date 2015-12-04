96a8795345b1b6159c2b9ee12ed3d42f8e32e573
FLY528 <97055699@qq.com>
2015-12-04 15:48:27 +0800

    12

--- 天天影视/天天影视/ViewController.m
+++ 天天影视/天天影视/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"wfl----hehe");
+    NSLog(@"卢恒傻吊");
 }
 
 - (void)didReceiveMemoryWarning {
